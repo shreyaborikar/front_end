89a9c3ed6601c84c9a5082932c7bd36102616348
shreyaborikar <92396167+shreyaborikar@users.noreply.github.com>
2023-12-22 23:31:59 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,9 @@ const About = () => {
   return (
     <Navbar>
       <h2>About Us</h2>
-      <p>Learn more about our yoga classes and the benefits of practicing yoga regularly.</p>
+      <p>Serene Yoga Studio offers a sanctuary for those seeking balance and rejuvenation through the practice of yoga. With its serene ambiance and dedicated instructors, the studio provides a holistic approach to well-being, seamlessly blending physical, mental, and spiritual elements.
+
+At Serene Yoga Studio, each class is meticulously curated to cater to practitioners of all levels, from beginners to seasoned yogis. The studio's experienced instructors guide participants through a harmonious blend of yoga postures, mindful breathing, and meditation, fostering a sense of inner peace and mindfulness. The classes are designed to promote flexibility, strength, and stress relief, creating a space for individuals to reconnect with their bodies and minds.</p>
       </Navbar>
   );
 }
